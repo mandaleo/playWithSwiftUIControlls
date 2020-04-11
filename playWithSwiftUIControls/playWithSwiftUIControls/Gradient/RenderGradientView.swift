5b4c4232e21c50cf793393367cb649000e2b0a4c
Manuel Martinez Gomez <manu.martinez.gomez@gmail.com>
2020-04-11 13:20:21 +0200

    Play with Gradients: Linear, Angular and Radial

--- playWithSwiftUIControls/playWithSwiftUIControls/Gradient/RenderGradientView.swift
+++ playWithSwiftUIControls/playWithSwiftUIControls/Gradient/RenderGradientView.swift
@@ -9,13 +9,53 @@
 import SwiftUI
 
 struct RenderGradientView: View {
-    var body: some View {
+  let colors = Gradient(colors:
+    [.red, .yellow, .green, .blue, .purple])
+  
+  var body: some View {
+    ScrollView {
+      VStack(spacing: 20) {
+        // Add LinearGradient Vertical
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+          .padding()
+          .foregroundColor(.white)
+          .background(LinearGradient(gradient: Gradient(colors: [.white, .black]), startPoint: .top, endPoint: .bottom))
+        
+        // Linear with 3 colors Vertical
+        Text("Hello World") .padding()
+          .foregroundColor(.white)
+          .background(LinearGradient(gradient: Gradient(colors: [.white, .red, .black]), startPoint: .top, endPoint: .bottom))
+        
+        // Liner horizontal
+        Text("Hello World") .padding()
+          .foregroundColor(.white)
+          .background(LinearGradient(gradient: Gradient(colors: [.white, .red, .black]), startPoint: .leading,
+                                     endPoint: .trailing))
+        
+        // Radial Gradient
+        Circle()
+          .fill(RadialGradient(gradient: self.colors,
+                               center: .center,
+                               startRadius: 50,
+                               endRadius: 200))
+          .frame(width: 400, height: 400)
+        
+        // Angular Gradient
+        Circle()
+          .fill(AngularGradient(gradient: colors, center: .center))
+          .frame(width: 400, height: 400)
+        
+        // Angular Gradient with strokeBorder
+        Circle()
+          .strokeBorder(AngularGradient(gradient: colors, center: .center, startAngle: .zero, endAngle: .degrees(360)), lineWidth: 50)
+          .frame(width: 400, height: 400)
+      }
     }
+  }
 }
 
 struct RenderGradientView_Previews: PreviewProvider {
-    static var previews: some View {
-        RenderGradientView()
-    }
+  static var previews: some View {
+    RenderGradientView()
+  }
 }
