53509815586b7af26329d7d18a2a55b80816da7d
Manuel Martinez Gomez <manu.martinez.gomez@gmail.com>
2020-05-01 16:59:38 +0200

    Add basic forms

---
+++ playWithSwiftUIControls/playWithSwiftUIControls/Layouts/Forms/BasicFormView.swift
@@ -0,0 +1,43 @@
+//
+//  BasicFormView.swift
+//  playWithSwiftUIControls
+//
+//  Created by Manuel Martinez Gomez on 01/05/2020.
+//  Copyright © 2020 Mandaleo. All rights reserved.
+//
+
+import SwiftUI
+
+struct BasicFormView: View {
+  @State private var enableLogging = false
+  @State private var selectedColor = 0
+  @State private var colors = ["Red", "Green", "Blue"]
+  
+  var body: some View {
+    NavigationView {
+      Form {
+        Picker(selection: $selectedColor, label: Text("Select a color")) {
+          ForEach(0 ..< colors.count) {
+            Text(self.colors[$0]).tag($0)
+          }
+        }.pickerStyle(SegmentedPickerStyle())
+        
+        Toggle(isOn: $enableLogging) {
+          Text("Enable Loggin")
+        }
+        
+        Button(action: {
+          // activate theme!
+        }) {
+          Text("Save changes")
+        }
+      }
+    }.navigationBarTitle("Settings")
+  }
+}
+
+struct BasicFormView_Previews: PreviewProvider {
+  static var previews: some View {
+    BasicFormView()
+  }
+}
