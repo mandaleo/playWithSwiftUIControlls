7b7423c85a793b019cb288280d34d72f37602ff7
Manuel Martinez Gomez <manu.martinez.gomez@gmail.com>
2020-06-24 15:27:17 +0200

    Built in shapes

---
+++ playWithSwiftUIControls/playWithSwiftUIControls/Drawing/Shapes/BuiltInShapesView.swift
@@ -0,0 +1,21 @@
+//
+//  BuiltInShapesView.swift
+//  playWithSwiftUIControls
+//
+//  Created by Manuel Martinez Gomez on 24/06/2020.
+//  Copyright © 2020 Mandaleo. All rights reserved.
+//
+
+import SwiftUI
+
+struct BuiltInShapesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BuiltInShapesView_Previews: PreviewProvider {
+    static var previews: some View {
+        BuiltInShapesView()
+    }
+}
